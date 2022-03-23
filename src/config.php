98bd582f97dfb1d9fb34ef0ee90e2aa91e116e80
OlaAni <b00134084@mytudublin.ie>
2022-03-23 10:38:49 +0000

    gitignore2

--- src/config.php
+++ src/config.php
@@ -2,6 +2,6 @@
 return array(
     'database_dsn' => 'mysql:dbname=comp_games;host=localhost',
     'database_user' => 'root',
-    'database_pass' => '',
+    'database_pass' => 'insertpasshere',
 
 );
