137b9b0effdd924a1250118b618f3f8abb16c544
b00132407 <91259835+b00132407@users.noreply.github.com>
2022-04-18 13:52:38 +0100

    register

--- public/register.php
+++ public/register.php
@@ -2,20 +2,26 @@
 require_once "../autoload.php";
 ?>
 <?php include "templates/registerHeader.php"; ?>
+
     <form method="post">
-        <label for="name">First Name</label>
-        <input type="text" name="name" id="name" required>
-        <label for="email">Email Address</label>
-        <input type="text" name="email" id="email" required>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required>
-        <label for="location">Location</label>
-        <input type="text" name="location" id="location">
-        <input type="submit" name="submit" value="Submit" required>
+        <div class="container">
+            <p>Please fill in this form to create an account.</p>
+            <hr>
+            <label for="name">First Name</label>
+            <input type="text" placeholder="Enter First Name" name="name" id="name" required>
+            <label for="email">Email Address</label>
+            <input type="text" placeholder="Enter Email" name="email" id="email" required>
+            <label for="password">Password</label>
+            <input type="password" placeholder="Enter Password" name="password" id="password" required>
+            <label for="location">Location</label>
+            <input type="text" placeholder="Enter Location" name="location" id="location">
+            <input type="submit" name="submit"  class="registerbtn" value="Submit" required>
+            <hr>
+        </div>
     </form>
 
 <form action="loginpage.php" method="post" class="navbar-form navbar-right">
-    <button name="login" value="login" class="button" type="submit">Login</button>
+    <button name="login" value="login" class="registerbtn" type="submit">Login</button>
 </form>
 
 
